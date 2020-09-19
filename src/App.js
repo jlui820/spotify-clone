74ad727e9fd52584d1ca7c1960d9de09eefbfbb9
Jeff <jlui820@gmail.com>
2020-09-19 16:08:19 -0700

    add spotifyWebApi

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import Login from './Login';
 import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
+
+const spotify = new SpotifyWebApi();
 
 function App() {
   const [token, setToken] = useState(null);
