3da46ecf6e760ae14dbfac25b837dc3820d71996
Jeff <jlui820@gmail.com>
2020-09-21 16:17:40 -0700

    create initialState and reducer in reducer.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { DataLayer }from './DataLayer';
+import { DataLayer } from './DataLayer';
+import reducer, { initialState } from './reducer';
 
 ReactDOM.render(
   <React.StrictMode>
-    <DataLayer initialState reducer> 
+    <DataLayer initialState={initialState} reducer={reducer}> 
       <App />
     </DataLayer>
   </React.StrictMode>,
