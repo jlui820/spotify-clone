e13495966a7858e10293e2bb05ccdbcbe75671da
Jeff <jlui820@gmail.com>
2020-09-21 17:11:25 -0700

    update reducer to include setting token and dispatch set token in app.js

--- src/App.js
+++ src/App.js
@@ -9,8 +9,7 @@ import { useDataLayerValue } from './DataLayer';
 const spotify = new SpotifyWebApi();
 
 function App() {
-  const [token, setToken] = useState(null);
-  const [{ user }, dispatch] = useDataLayerValue();
+  const [{ user, token }, dispatch] = useDataLayerValue();
 
   useEffect(() => {
     const hash = getTokenFromUrl();
@@ -18,7 +17,11 @@ function App() {
     const _token = hash.access_token;
 
     if (_token) {
-      setToken(_token);
+
+        dispatch({
+          type: 'SET_TOKEN',
+          token: _token,
+        });
 
       spotify.setAccessToken(_token);
 
@@ -29,10 +32,7 @@ function App() {
         });
       });
     }
-
-    console.log('my token ==>', token);
   }, []);
-  console.log('this is the user', user)
 
   return (
     <div className="app">
--- src/reducer.js
+++ src/reducer.js
@@ -2,7 +2,10 @@ export const initialState = {
   user: null,
   playlists: [],
   playing: false,
-  item: null
+  item: null,
+  //REMOVE AFTER FINISH DEVELOPING AND CHANGE BACK TO NULL;
+  token:
+    'BQB-inqRlKY2QVx7KSF_o6ZzIaYRBPs2zE8GAIMiEyFb_0UNV8D89cy-L9jVltcdPuzJrTgnbXwvM1fVK5IZxnyRpi-GJx0AmId7wmjqHLqMsZ42PhES5fNelXzk6rOm1NVqulH8vu7rj3GgFcH9vp7DJdM0',
 };
 
 const reducer = (state, action) => {
@@ -14,6 +17,12 @@ const reducer = (state, action) => {
         ...state,
         user: action.user
       };
+
+    case 'SET_TOKEN':
+      return {
+        ...state,
+        token: action.token,
+      }
     
     default:
       return state;
