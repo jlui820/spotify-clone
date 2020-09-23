732dba4275a1dc897a7e216e5184f31be379086e
Jeff <jlui820@gmail.com>
2020-09-23 14:42:44 -0700

    add icons and styling to footer center

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
+import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
+import SkipNextIcon from "@material-ui/icons/SkipNext";
+import ShuffleIcon from "@material-ui/icons/Shuffle";
+import RepeatIcon from "@material-ui/icons/Repeat";
+import VolumeDownIcon from "@material-ui/icons/VolumeDown";
+import PauseCircleOutlineIcon from "@material-ui/icons/PauseCircleOutline";
+import PlaylistPlayIcon from "@material-ui/icons/PlaylistPlay";
 import './Footer.css';
 
 function Footer() {
   return (
     <div className='footer'>
-      <h1>This is the footer</h1>
+      <div className="footer__left">
+        <p>Album and song details</p>
+      </div>
+      <div className="footer__center">
+        <ShuffleIcon className='footer__green' />
+        <SkipPreviousIcon className='footer__icon' />
+        <PlayCircleOutlineIcon fontSize='large' className='footer__icon' />
+        <SkipNextIcon className='footer_icon' />
+        <RepeatIcon className='footer_green' />
+      </div>
+      <div className="footer__right">
+        <p>Volume controls</p>
+      </div>
     </div>
   )
 }
